d58fa6c58fd7cbbfdcd03590cc306d44e35e0ce3
Aboubacar Sidik Moussa Garba <aboubacarsidikmoussagarba8720@gmail.com>
2026-09-26 00:00:02 +0100

    Bump Kali VNC to 2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.kalivnc.app"
         minSdk = 26
         targetSdk = 28
-        versionCode = 13
-        versionName = "2.0"
+        versionCode = 14
+        versionName = "2.1"
     }
 
     buildTypes {
